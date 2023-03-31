f1461cfb789f97c0da24c07e919cf3154dbf7610
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-01 07:59:45 +0800

    step:index renew

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose=require('mongoose')
 const exphbs = require('express-handlebars')
-const restaurantList=require('./restaurant.json')
+const RestData=require('./models/restaurantData')
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -28,27 +28,14 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))//在 Express 中提供靜態檔案
 
 app.get('/',(req,res)=>{
-  res.render('index',{restaurants:restaurantList.results})
+  RestData.find()
+  .lean()
+  .then(restaurants=>res.render('index',{restaurants}))
+  .catch(error => console.error(error))
 })
 
-app.get('/restaurants/:restaurants_id',(req,res)=>{
-  const filterRestaurant=restaurantList.results.find(restaurant=>
-    restaurant.id.toString()===req.params.restaurants_id
-  )
-  res.render('show',{restaurant:filterRestaurant})}
-)
 
-app.get('/search',(req,res)=>{const keyword=req.query.keyword
-  const restaurants=restaurantList.results.filter(restaurant=>{
-    return restaurant.name.toLowerCase().includes(keyword)||restaurant.category.includes(keyword)} )
-  if(restaurants.length===0){res.render('wrong',{keyword:keyword})}
-  else{res.render('index', {restaurants:restaurants, keyword: keyword})}
-  
-  
-  }
-)
 
 app.listen(port,()=>{
   console.log(`Express is listening on localhost:${port}`)
 })
-
