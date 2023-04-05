e411b8603b4a6dc042ed7d1869bb3416fba7daa2
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-05 08:22:26 +0800

    module.exports = db

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const RestData = require('./models/restaurantData')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
 
 const port = 3000
 
 const app = express()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
