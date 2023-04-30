65981fc89f07993fedac80948caae116d8f2ed9a
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-30 18:00:38 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const RestData = require('./models/restaurantData')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const session = require('express-session')
 require('./config/mongoose')
 
 const port = 3000
@@ -15,6 +16,11 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))// 在 Express 中提供靜態檔案
 app.use(express.urlencoded({ extended: true }))// 使用 body-parser
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
 
 app.listen(port, () => {
