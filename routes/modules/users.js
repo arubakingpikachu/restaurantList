a4a8104a0d5519be6d61a42db3ac83370930df15
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-30 16:58:04 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const userData=require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -9,4 +11,31 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')})
+
+router.post('/register',(req,res)=>{
+  const {name,email,password,confirmPassword}=req.body
+  userData.findOne({email})
+  .then(user=>{
+    if(user){
+      res.render('register',{
+        name:name,
+        email:email,
+        password:password,
+        confirmPassword:confirmPassword
+      })
+    }else{
+      return userData.create({
+        name:name,
+        email:email,
+        password:password,
+        confirmPassword:confirmPassword
+      })
+      .then(()=>res.redirect('/'))
+      .catch(error => console.error(error))
+    }
+  })
+  .catch(error => console.error(error))
+})
+
+  
 module.exports = router
